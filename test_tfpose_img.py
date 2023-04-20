678a660c08734147c7060d1a3be5e8702e5750fc
John Dojjy Yeung <cfyeung2357@gmail.com>
2023-04-20 21:01:08 +0800

    added movenet model for multi-person detection, also added other test files for testing and reference

---
+++ test_tfpose_img.py
@@ -0,0 +1,27 @@
+# Import TF and TF Hub libraries.
+import tensorflow as tf
+
+# Load the input image.
+image_path = 'test3.jpg'
+image = tf.io.read_file(image_path)
+image = tf.compat.v1.image.decode_jpeg(image)
+image = tf.expand_dims(image, axis=0)
+# Resize and pad the image to keep the aspect ratio and fit the expected size.
+image = tf.image.resize_with_pad(image, 192, 192)
+
+# Initialize the TFLite interpreter
+interpreter = tf.lite.Interpreter(model_path="models/TFLite_float16.tflite")
+interpreter.allocate_tensors()
+
+# TF Lite format expects tensor type of float32.
+input_image = tf.cast(image, dtype=tf.uint8)
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+interpreter.set_tensor(input_details[0]['index'], input_image.numpy())
+
+interpreter.invoke()
+
+# Output is a [1, 1, 17, 3] numpy array.
+keypoints_with_scores = interpreter.get_tensor(output_details[0]['index'])
+print(keypoints_with_scores)
