423fe5b2d8c4d92e0d38e30ab2b02844ae5f7b76
John Yeung <cfyeung2357@gmail.com>
2023-04-21 17:19:38 +0800

    Added class for classifing action from collected pose information

---
+++ ActionClassifier.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+
+Actions = {
+    0: "walk",
+    1: "run"
+}
+
+class PoseActionClassifier:
+    def __init__(self):
+        pass
+
+    def classify(self, pose):
+        pass
