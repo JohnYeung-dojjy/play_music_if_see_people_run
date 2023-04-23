1f90b429a560fc8e28161d8cc8bf7d9cd251430a
John Dojjy Yeung <cfyeung2357@gmail.com>
2023-04-23 11:07:48 +0800

    change default tab size to 2 for more concise display

--- ActionClassifier.py
+++ ActionClassifier.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 
 Actions = {
-    0: "walk",
-    1: "run"
+  0: "walk",
+  1: "run"
 }
 
 class PoseActionClassifier:
-    def __init__(self):
-        pass
+  def __init__(self):
+    pass
 
-    def classify(self, pose):
-        pass
+  def classify(self, pose):
+    pass
--- PoseDetector.py
+++ PoseDetector.py
@@ -9,69 +9,77 @@ from typing import NamedTuple
 import tensorflow as tf
     
 class PoseDetectionResult:
-    def __init__(self, results: NamedTuple, mask: set=set()):
-        # if mask is None:
-        #     # discard 0-10 for head positions
-        #     mask = {
-        #         PoseLandmark.NOSE,
-        #         PoseLandmark.LEFT_EYE_INNER,
-        #         PoseLandmark.LEFT_EYE,
-        #         PoseLandmark.LEFT_EYE_OUTER,
-        #         PoseLandmark.RIGHT_EYE_INNER,
-        #         PoseLandmark.RIGHT_EYE,
-        #         PoseLandmark.RIGHT_EYE_OUTER,
-        #         PoseLandmark.LEFT_EAR,
-        #         PoseLandmark.RIGHT_EAR,
-        #         PoseLandmark.MOUTH_LEFT,
-        #         PoseLandmark.MOUTH_RIGHT
-        #     }
-        # landmark.x, landmark.y are in a [0,1] scale, except when those are out-of-bound
-        self.landmarks: list = list([[landmark.x, landmark.y] for (idx, landmark) in enumerate(results.pose_landmarks.landmark) if idx not in mask]) # type: ignore
+  def __init__(self, landmarks: list[float], mask: set=set()):
+    # if mask is None:
+    #     # discard 0-10 for head positions
+    #     mask = {
+    #         PoseLandmark.NOSE,
+    #         PoseLandmark.LEFT_EYE_INNER,
+    #         PoseLandmark.LEFT_EYE,
+    #         PoseLandmark.LEFT_EYE_OUTER,
+    #         PoseLandmark.RIGHT_EYE_INNER,
+    #         PoseLandmark.RIGHT_EYE,
+    #         PoseLandmark.RIGHT_EYE_OUTER,
+    #         PoseLandmark.LEFT_EAR,
+    #         PoseLandmark.RIGHT_EAR,
+    #         PoseLandmark.MOUTH_LEFT,
+    #         PoseLandmark.MOUTH_RIGHT
+    #     }
+    # landmark.x, landmark.y are in a [0,1] scale, except when those are out-of-bound
+    #TODO: make it np array by default
+    self.landmarks: list = list([[landmark.x, landmark.y] for (idx, landmark) in enumerate(landmarks.landmark) if idx not in mask]) # type: ignore
+  #TODO: make normalize function    
         
-        
-    def to_torch_tensor(self):
-        return torch.tensor(self.landmarks)
-    
-    def to_numpy(self):
-        return np.array(self.landmarks)
+  def to_torch_tensor(self):
+    return torch.tensor(self.landmarks)
+  
+  def to_numpy(self):
+    return np.array(self.landmarks)
+  
+  def to_flattened_list(self):
+    return list(self.to_numpy().flatten())
 
 class SinglePersonPoseDetector():
-    def __init__(self,
-                 static_image_mode=False,
-                 min_detection_confidence=0.5,
-                 min_tracking_confidence=0.5):
-        
-        self.pose: Pose = Pose(static_image_mode=static_image_mode,
-                         min_detection_confidence=min_detection_confidence,
-                         min_tracking_confidence=min_tracking_confidence)
-        
-    def detect(self, image)->list:
-        #TODO: return list of landmarks, instead of image
-        image_height, image_width, _ = image.shape
-        results = self.pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-        if results.pose_landmarks: # type: ignore
-            for landmark in results.pose_landmarks.landmark: # type: ignore
-                landmark_x = min(int(landmark.x * image_width), image_width - 1)
-                landmark_y = min(int(landmark.y * image_height), image_height - 1)
-                cv2.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), -1)
-        draw_landmarks(
-        image,
-        results.pose_landmarks, # type: ignore
-        list(POSE_CONNECTIONS),
-        landmark_drawing_spec=get_default_pose_landmarks_style())
-        return image
-    
+  def __init__(self,
+              static_image_mode=False,
+              min_detection_confidence=0.5,
+              min_tracking_confidence=0.5):
+      
+    self.pose: Pose = Pose(static_image_mode=static_image_mode,
+                        min_detection_confidence=min_detection_confidence,
+                        min_tracking_confidence=min_tracking_confidence)
+      
+  def detect(self, image)->PoseDetectionResult|None:
+    #TODO: return list of landmarks, instead of image
+    # image_height, image_width, _ = image.shape
+    results = self.pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    # print(results.pose_landmarks)
+    if results.pose_landmarks: # type: ignore
+      return PoseDetectionResult(results.pose_landmarks) # type: ignore
+    else:
+      return None
+    # if results.pose_landmarks: # type: ignore
+    #     for landmark in results.pose_landmarks.landmark: # type: ignore
+    #         landmark_x = min(int(landmark.x * image_width), image_width - 1)
+    #         landmark_y = min(int(landmark.y * image_height), image_height - 1)
+    #         cv2.circle(image, (landmark_x, landmark_y), 5, (0, 255, 0), -1)
+    # draw_landmarks(
+    # image,
+    # results.pose_landmarks, # type: ignore
+    # list(POSE_CONNECTIONS),
+    # landmark_drawing_spec=get_default_pose_landmarks_style())
+    # return image
+  
 def test():
-    from VideoStreamManager import VideoStreamManager
-    video_stream = VideoStreamManager(camera_id=0)
-    pose_detector = SinglePersonPoseDetector()
-    for frame in video_stream.read_frames():
-        
-        cv2.imshow('frame', pose_detector.detect(frame))
-    
+  from VideoStreamManager import VideoStreamManager
+  video_stream = VideoStreamManager(camera_id=0)
+  pose_detector = SinglePersonPoseDetector()
+  for frame in video_stream.read_frames():
+      
+    cv2.imshow('frame', pose_detector.detect(frame))
+  
     
 
 
 if __name__ == '__main__':
-    test()
+  test()
