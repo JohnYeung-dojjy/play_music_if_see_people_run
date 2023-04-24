eef951d4ae24af17fbaf5260af7178e0606a4f12
John Yeung <cfyeung2357@gmail.com>
2023-04-24 12:53:39 +0800

    module init

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""This module contains classes and methods for human pose action detection."""
+from AudioPlayer import AudioPlayer
+from PoseDetector import SinglePersonPoseDetector, PoseDetectionResult
+from VideoStreamManager import VideoStreamManager
+import app
+
+
