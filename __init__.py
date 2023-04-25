a439926f4789bff3456fdb4c049b398b41b9f269
John Yeung <cfyeung2357@gmail.com>
2023-04-25 11:54:45 +0800

    export all classes

--- __init__.py
+++ __init__.py
@@ -2,6 +2,10 @@
 from AudioPlayer import AudioPlayer
 from PoseDetector import SinglePersonPoseDetector, PoseDetectionResult
 from VideoStreamManager import VideoStreamManager
+from ActionClassifier import PoseActionClassifier
+from TrainingData import load_KTH
+
+
 import app
 
 
